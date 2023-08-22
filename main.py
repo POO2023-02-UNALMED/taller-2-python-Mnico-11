41a3d4b08226a10ccf9674ff9e6a963eb0758d84
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2023-08-22 08:33:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Motor:
 
 class Auto:
     cantidadCreados = 0
-    def __init__(self, modelo, precio, asientos, marca, motor, resgitro) :
+    def __init__(self, modelo, precio, asientos, marca, motor, registro) :
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
