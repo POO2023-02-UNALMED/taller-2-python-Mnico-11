e27d5708f78d173dea4165618e22d4a7b76064f8
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2023-08-22 08:26:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Auto:
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        Auto.cantidadCreados =+1
+    
     
     def cantidadAsientos(self):
         numeroAsientos=0
